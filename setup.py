cb29e135e1f5fa675bb7bebb84653974c6001a2f
Sourav Anil Hawaldar <sourav.hawaldar@gmail.com>
2026-03-26 09:05:15 +0100

    Revert "prepare repo files"
    This reverts commit 132cb67311295d09c4dd3a4fd1a4b82f3bec34a1.

--- setup.py
+++ setup.py
@@ -1,40 +1,32 @@
 from setuptools import find_packages, setup
-import os
 
 package_name = 'ros2_studio'
-
-# Collect all image files in the package directory
-data_files = [
-    ('share/ament_index/resource_index/packages',
-        ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    ('share/' + package_name, ['README.md']),
-]
-
-# Include any images in the package directory
-image_files = [
-    f for f in os.listdir(package_name)
-    if f.endswith(('.png', '.jpg', '.jpeg', '.gif'))
-] if os.path.isdir(package_name) else []
-
-if image_files:
-    data_files.append(
-        ('share/' + package_name + '/images',
-         [os.path.join(package_name, f) for f in image_files])
-    )
 
 setup(
     name=package_name,
     version='0.1.0',
     packages=find_packages(exclude=['test']),
-    data_files=data_files,
-    install_requires=['setuptools'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'PyQt5',
+        'psutil',
+        'matplotlib',
+    ],
     zip_safe=True,
-    maintainer='Sourav Hawaldar',
+    maintainer='sourav',
     maintainer_email='sourav.hawaldar@gmail.com',
     description='Comprehensive ROS2 monitoring and management tool with GUI',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'ros2cli.command': [
             'studio = ros2_studio.command.studio:StudioCommand',
